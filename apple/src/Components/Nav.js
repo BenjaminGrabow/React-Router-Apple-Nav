c1480327668cea32409dfc5b59a9c31467383c76
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-11 14:10:15 +0200

    Feature: added navData file

--- apple/src/Components/Nav.js
+++ apple/src/Components/Nav.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Nav = (props) => {
-        return (
-                <div>
-                <NavLink>Apple</NavLink>
-                <NavLink>Mac</NavLink>
-                <NavLink>iPad</NavLink>
-                <NavLink>iPhone</NavLink>
-                <NavLink>Watch</NavLink>
-                <NavLink>TV</NavLink>
-                <NavLink>Music</NavLink>
-                <NavLink>Support</NavLink>
-                <NavLink>Search</NavLink>
-                <NavLink>Window</NavLink>
-                </div>
-);
+const StyledDiv = styled.div`
+
+a {
+        margin: 1rem;
 }
 
+`;
+
+class Nav extends Component {
+        constructor(props) {
+                super(props);
+                this.state = { 
+                        nav: navData
+                 }
+        }
+        render() { 
+                return ( 
+                        
+                 );
+        }
+}
+ 
 export default Nav;
